b302ceafd560197b05f43a74ad69964f8e987615
pangara <pangara@uvic.ca>
2018-03-03 15:01:21 -0800

    readd neighbors

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ def start():
 def move():
     data = bottle.request.json
     return {
-        print "game id-------------" games[data[id]]
+        print "game id-------------", games[data[id]]
         'move': games[data["id"]].move(data),
         'taunt': 'Good ol sneakeys baaack'
     }
